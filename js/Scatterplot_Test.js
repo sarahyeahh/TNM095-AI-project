8e304699e0c4d8338e7f7438da75428c7cc05d66
Hanna Johansson <hanna93.hej@gmail.com>
2017-10-09 13:39:15 +0200

    created functions in the file

--- js/Scatterplot_Test.js
+++ js/Scatterplot_Test.js
@@ -4,26 +4,22 @@
 
  	From index2.html
 
+    - createDataSet()
+    - createSVG()
+    - createCircles()
+    - drawElevator()
+    - drawStairs()
+    - drawAxes()
+
 ***********************************************************************************************************/
 
- // Setup data
-
-//Plottar ut massa random värden. 
-var dataset = [];  // Initialize empty array
-var numDataPoints = 15;  // Number of dummy data points
-
-//xval = initial position for the dots
-//yval = initial position for the dots
-for(var i = 0; i < numDataPoints; i++) {
-    var xval = 60; //Math.floor(Math.random() * maxRange);  // New random integer  
-    var yval = 0; //Math.floor(Math.random() * maxRange);  // New random integer
-    dataset.push([xval, yval]);  // Add new number to array
-}
 
 // Setup settings for graphic
 var canvas_width = 700;
 var canvas_height = 500;
 var padding = 30;  // for chart edges
+
+var dataset = createDataSet();
 
 // Create scale functions
 var xScale = d3.scale.linear()  // xScale is width of graphic
@@ -46,45 +42,93 @@ var yAxis = d3.svg.axis()
                 .orient("left")
                 .ticks(5);
 
-// Create SVG element
-var svg = d3.select("h4")  // This is where we put our vis
-    .append("svg")
-    .attr("width", canvas_width)
-    .attr("height", canvas_height)
+var svg = createSVG();
 
-// Create Circles
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")  // Add circle svg
-    .attr("cx", function(d) {
-        return xScale(d[0]);  // Circle's X
-    })
-    .attr("cy", function(d) {  // Circle's Y
-        return yScale(d[1]);
-    })
-    .attr("r", 2);  // radius
+createCircles(svg);     //Create the circles
+drawElevator(160, 20);  //Draw an elevator at this position
+drawStairs(10,120);     //Draw the stairs at this position
+drawAxes(svg);        //Draw the axes. If this function is not called, no axes are visible.
 
-//Draw rectangle --> elevator
-var rectangle = svg.append('rect')
-      .attr('x', 40)
-      .attr('y', 20)
+
+// Setup data
+function createDataSet() {
+    //Plottar ut massa random värden. 
+    var dataset = [];  // Initialize empty array
+    var numDataPoints = 15;  // Number of dummy data points
+
+    //xval and yval = the initial position for the dots
+    for(var i = 0; i < numDataPoints; i++) {
+        var xval = 60; //Math.floor(Math.random() * maxRange);  // New random integer  
+        var yval = 0; //Math.floor(Math.random() * maxRange);  // New random integer
+        dataset.push([xval, yval]);  // Add new number to array
+    }
+
+    return dataset;
+}
+
+
+// Create the SVG element
+function createSVG () {
+    // "h4" is where we put our visualization
+    var svg = d3.select("h4")  
+        .append("svg")
+        .attr("width", canvas_width)
+        .attr("height", canvas_height)
+
+    return svg;
+}
+
+
+//Create the circles
+function createCircles (svg) {
+    // Create Circles
+    svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")  // Add circle svg
+        .attr("cx", function(d) {
+            return xScale(d[0]);   // Circle's X position
+        })
+        .attr("cy", function(d) {  // Circle's Y position
+            return yScale(d[1]);
+        })
+        .attr("r", 2);  // radius
+}
+
+
+//Draw the elevator --> a rectangle, a blue rectangle
+function drawElevator(posX, posY) {
+    var rectangle = svg.append('rect')
+      .attr('x', posX)
+      .attr('y', posY)
       .attr('width', 80)
       .attr('height', 20)
       .style('fill', 'rgb(0,0,255)')
+}
 
+//Draw the stairs --> a rectangle, a green rectangle
+function drawStairs(posX, posY) {
+    var rectangle = svg.append('rect')
+      .attr('x', posX)
+      .attr('y', posY)
+      .attr('width', 20)
+      .attr('height', 80)
+      .style('fill', 'rgb(0,255,0)')
+}
 
-// Add to X axis
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + (canvas_height - padding) +")")
-    .call(xAxis);
+function drawAxes(svg) {
+    // Add to X axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (canvas_height - padding) +")")
+        .call(xAxis);
 
-// Add to Y axis
-svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(" + padding +",0)")
-    .call(yAxis);
+    // Add to Y axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + padding +",0)")
+        .call(yAxis);
+}
 
 // On click, update with new data
 var starta = document.getElementById("klickahär");
@@ -96,7 +140,7 @@ d3.select(starta)
         dataset = [];  // Tom array med alla x och y värden.
 
         for(var i = 0; i<numValues; i++) {
-            var xval = 50 + 5*i;//Math.floor(Math.random() * maxRange);  // Random int for x
+            var xval = 40 + 5*i;//Math.floor(Math.random() * maxRange);  // Random int for x
             var yval = 100; //Math.floor(Math.random() * maxRange);  // Random int for y
 
             //Returnerar det nya x och y värdet. 
@@ -115,7 +159,7 @@ d3.select(starta)
         svg.selectAll("circle")
             .data(dataset)  // Update with new data
             .transition()  // Transition from old to new
-            .duration(1000)  // Length of animation
+            .duration(4000)  // Length of animation, default = 1000
             .each("start", function() {  // Start animation
                 d3.select(this)  // 'this' means the current element
                     .attr("fill", "red")  // Change color
