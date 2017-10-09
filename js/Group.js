5163d7a1dfe06cb9097ec16f2f1ec2ebfb2b4e05
Hanna Johansson <hanna93.hej@gmail.com>
2017-10-09 18:11:22 +0200

    similar to People, but just tested with a new object

---
+++ js/Group.js
@@ -0,0 +1,14 @@
+/**********************************************************************************************************
+ 	Authors:  Sarah Fosse and Hanna Johansson
+ 	Date: Created 2017-10-09
+ 
+***********************************************************************************************************/
+
+function Group() {
+	this.stressed = 1;
+	this.tired = 1;
+	//this.waitTime = 1;
+	//this.speed = 2; 
+	this.ID = -1;
+	this.groupSize = People.prototype.generatePeople();
+}
